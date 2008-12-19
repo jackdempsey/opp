578f133373d04365993d6de0324a7c1d13052036
Jack Dempsey <jack.dempsey@gmail.com>
2008-12-19 17:11:13 -0500

    do nothing to fields that aren't there

--- spec/opp_spec.rb
+++ spec/opp_spec.rb
@@ -3,6 +3,8 @@ require 'rubygems'
 require 'spec'
 require 'sequel'
 
+DB = Sequel.sqlite
+
 describe "opp" do
   it "adds opp_filter_out to Sequel::Models" do
     lambda do
@@ -10,5 +12,25 @@ describe "opp" do
         opp_filter_out :an_ugly_field
       end
     end.should_not raise_error
+  end
+
+  it "does nothing to a field that doesn't exist" do
+    DB.create_table :example do
+      primary_key :id
+      varchar :name
+    end
+
+    class Bar < Sequel::Model(:example)
+    end
+
+    before_bar_inspect = Bar.new(:name => 'bar').inspect
+
+    class Bar < Sequel::Model(:example)
+      opp_filter_out :nonexistent_field
+    end
+
+    after_bar_inspect = Bar.new(:name => 'bar').inspect
+
+    before_bar_inspect.should == after_bar_inspect
   end
 end
